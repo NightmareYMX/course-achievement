cc8db5f2bbfa967af162a1ffcf8d6563cbfc519d
Lee <2501551375@qq.com>
2022-10-19 09:30:43 +0800

    update .gitignore

--- src/main/java/com/school/course_achievement/mapper/StudentMapper.java
+++
@@ -1,9 +0,0 @@
-package com.school.course_achievement.mapper;
-
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface StudentMapper {
-
-    int findUser(String username, String password);
-}
