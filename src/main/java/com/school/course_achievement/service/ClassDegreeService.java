1fca296f5bbe119edfa7b9d1760e84eaa6dc0939
ymx <1878985221@qq.com>
2022-10-24 20:15:14 +0800

    平时成绩后端接口

--- src/main/java/com/school/course_achievement/service/ClassDegreeService.java
+++ src/main/java/com/school/course_achievement/service/ClassDegreeService.java
@@ -1,13 +1,12 @@
 package com.school.course_achievement.service;
 
-import com.school.course_achievement.pojo.ClassDegree;
-
 import java.util.List;
+import java.util.Map;
 
 public interface ClassDegreeService {
     /**
-     * 按课程号查询所有的班级分数
-     * @return 所有班级的所有成绩信息
+     * 按课程号查询所有的班级平时成绩
+     * @return 所有班级的所有平时成绩信息
      */
-    List<ClassDegree> getAllClassDegree(String KNo);
+    List<Map<String, Double>> getAllClassDegree(String KNo);
 }
--- src/main/java/com/school/course_achievement/service/impl/ClassDegreeServiceImpl.java
+++ src/main/java/com/school/course_achievement/service/impl/ClassDegreeServiceImpl.java
@@ -1,5 +1,6 @@
 package com.school.course_achievement.service.impl;
 import com.school.course_achievement.mapper.ClassDegreeMapper;
+import com.school.course_achievement.mapper.WeightMapper;
 import com.school.course_achievement.pojo.ClassDegree;
 import com.school.course_achievement.pojo.ClassDegreeExample;
 import com.school.course_achievement.service.ClassDegreeService;
@@ -7,21 +8,29 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 @Transactional
 public class ClassDegreeServiceImpl implements ClassDegreeService {
-    List<ClassDegree> classDegreeList;
+
     @Autowired
     ClassDegreeMapper classDegreeMapper;
 
+    @Autowired
+    WeightMapper weightMapper;
     @Override
-    public List<ClassDegree> getAllClassDegree(String KNo) {
+    public List<Map<String, Double>> getAllClassDegree(String KNo) {
+        List<Map<String, Double>> degreeMapList = new ArrayList<>();
         ClassDegreeExample classDegreeExample = new ClassDegreeExample();
         ClassDegreeExample.Criteria criteriaClassDegreeExample = classDegreeExample.createCriteria();
         criteriaClassDegreeExample.andKNoEqualTo(KNo);
-        classDegreeList = classDegreeMapper.selectByExample(classDegreeExample);
-        return classDegreeList;
+        List<ClassDegree> classDegreeList = classDegreeMapper.selectByExample(classDegreeExample);
+        Map<String, Double> degreeMap = new HashMap<>();
+        //未完成
+        return degreeMapList;
     }
 }
