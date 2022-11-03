17351f16f133fdda26a796ad60cb20670e476825
ymx <1878985221@qq.com>
2022-11-03 11:22:03 +0800

    系数修改是否等于1的判断和提示信息的显示

--- src/main/java/com/school/course_achievement/controller/weightController.java
+++ src/main/java/com/school/course_achievement/controller/weightController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @Controller
@@ -38,8 +39,34 @@ public class weightController {
     }
 
     @RequestMapping(value = "/update", method = RequestMethod.PUT)
-    public String update(Weight weight) {
-        weightService.updateWeight(weight);
-        return "redirect:/weight/getAllWeight/1";
+    public String update(Weight weight, HttpSession session) {
+        Double behaveTarget1Weight = weight.getBehaveTarget1Weight();
+        Double behaveTarget2Weight = weight.getBehaveTarget2Weight();
+        Double behaveTarget3Weight = weight.getBehaveTarget3Weight();
+        Double homeworkTarget1Weight = weight.getHomeworkTarget1Weight();
+        Double homeworkTarget2Weight = weight.getHomeworkTarget2Weight();
+        Double homeworkTarget3Weight = weight.getHomeworkTarget3Weight();
+        Double testTarget1Weight = weight.getTestTarget1Weight();
+        Double testTarget2Weight = weight.getTestTarget2Weight();
+        Double testTarget3Weight = weight.getTestTarget3Weight();
+        Double finalTarget1Weight = weight.getFinalTarget1Weight();
+        Double finalTarget2Weight = weight.getFinalTarget2Weight();
+        Double finalTarget3Weight = weight.getFinalTarget3Weight();
+        Double totalTarget1Weight = weight.getTotalTarget1Weight();
+        Double totalTarget2Weight = weight.getTotalTarget2Weight();
+        Double totalTarget3Weight = weight.getTotalTarget3Weight();
+        if (behaveTarget1Weight + behaveTarget2Weight + behaveTarget3Weight != 1
+                || homeworkTarget1Weight + homeworkTarget2Weight + homeworkTarget3Weight != 1
+                || testTarget1Weight + testTarget2Weight + testTarget3Weight != 1
+                || finalTarget1Weight + finalTarget2Weight + finalTarget3Weight != 1
+                || totalTarget1Weight + totalTarget2Weight + totalTarget3Weight != 1
+        ) {
+            session.setAttribute("updateWeightMsg", "修改失败，三个目标权重之和必须等于1!");
+            return String.format("redirect:/weight/toUpdate/%s", weight.getkNo());
+        } else {
+            session.removeAttribute("updateWeightMsg");
+            weightService.updateWeight(weight);
+            return "redirect:/weight/getAllWeight/1";
+        }
     }
 }
