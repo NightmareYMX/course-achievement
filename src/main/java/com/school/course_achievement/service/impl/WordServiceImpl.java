785c1145ab0dc955a93fc27accccf6908ea33030
ymx <1878985221@qq.com>
2022-11-05 21:32:38 +0800

    初始化成绩更改

--- src/main/java/com/school/course_achievement/service/impl/WordServiceImpl.java
+++ src/main/java/com/school/course_achievement/service/impl/WordServiceImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class wordServiceImpl implements WordService {
+public class WordServiceImpl implements WordService {
     @Autowired
     CourseMapper courseMapper;
     @Override
